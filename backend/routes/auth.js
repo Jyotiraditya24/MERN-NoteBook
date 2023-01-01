2cecf985922d56b32f53d1c463a27211e9be8e79
jyotiraditya <jyotiraditya24@gmail.com>
2023-01-02 00:38:23 +0530

    Authentication Done 4.0

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const { response } = require('express');
 
 // Creates a User using:POST "/api/auth" . does not require authentication
 
-router.post('/',(req,resp)=>{
-   console.log(req.body);
-   const user = new User(req.body);
-   user.save();
-   resp.send(user); 
+router.post('/',[
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email','Enter a valid Email').isEmail(),
+    body('password','Enter a valid password').isLength({min:5}),
+],(req,res)=>{
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+   User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+   }).then(user=>res.json(user))
+   .catch(error=>res.json(error))
 })
 
 module.exports = router;
